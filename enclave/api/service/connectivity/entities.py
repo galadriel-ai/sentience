ceba8ebc0a419ef6e9fc2edbbe563a7d5b4c675c
kgrofelnik <kresimir.grofelnik@gmail.com>
2024-12-20 09:55:42 +0100

    Removes Solana checks

--- enclave/api/service/connectivity/entities.py
+++ enclave/api/service/connectivity/entities.py
@@ -4,5 +4,3 @@ from pydantic import Field
 
 class CheckConnectivityResponse(BaseModel):
     openai: bool = Field(description="OpenAI connectivity status")
-    solana_mainnet: bool = Field(description="Solana Mainnet connectivity status")
-    solana_devnet: bool = Field(description="Solana Devnet connectivity status")
